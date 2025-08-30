881542800141e84901bff992296b2157238f1f90
Ger Hobbelt <ger@hobbelt.com>
2025-08-30 15:01:32 +0200

    feature: moving everyone to a `std::breakpoint_if_debugging()` style approach, where `DEBUG_BREAK_H()` does not cause the running application to b0rk when NO debugger is attached.
    Of course there are issues: only two of the major platforms provide a reasonable/known way to detect the presence of an actual debugger, so we have to resort to "heuristics" shenanigans for the others: we will assume any DEBUG BUILD (`#define _DEBUG`) will expect a debugger and thus produce an active `debug_break()`, while any non-debug, i.e _release_ build is assumed to NOT have a debugger attached.
    
    TODO: provide a simple user-definable override to kill this mess: the application code writer knows best, in the end.  ;-)

--- debugbreak.h
+++ debugbreak.h
@@ -129,6 +129,195 @@
 #define DEBUG_BREAK_USE_INTRINSIC_MSVC_DEBUGBREAK   6
 #define DEBUG_BREAK_USE_STD_DEBUGBREAK              7
 
+// ---------------------------------------------------------------------------------
+//
+// definition of `debugbreak_is_debugger_present()`
+
+#if defined(_WIN32)
+
+//#include <winsock2.h>
+//#include <windows.h>
+
+#if !defined(_APISETDEBUG_) || !defined(WINAPI_FAMILY_PARTITION) /* || !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP | WINAPI_PARTITION_SYSTEM | WINAPI_PARTITION_GAMES) */
+// mandatory Win32 APIs not defined yet, so we do it here:
+
+// #if (defined(_WIN32_WINNT) && (_WIN32_WINNT >= 0x0400)) || (defined(_WIN32_WINDOWS) && (_WIN32_WINDOWS > 0x0400))
+
+DEBUGBREAK_EXTERN_C
+extern __declspec(dllimport) void __stdcall DebugBreak(void);
+DEBUGBREAK_EXTERN_C
+extern __declspec(dllimport)  int __stdcall IsDebuggerPresent(void);
+
+#endif
+
+DEBUGBREAK_EXTERN_C
+static inline int debugbreak_is_debugger_present(void)
+{
+	return IsDebuggerPresent();
+}
+
+#elif __has_include(<debugging>) && __has_feature(__cpp_lib_debugging)
+
+#include <debugging>
+
+DEBUGBREAK_EXTERN_C
+static inline int debugbreak_is_debugger_present(void)
+{
+	return std::is_debugger_present();
+}
+
+#elif (defined(__APPLE__) || defined(__apple__)) && defined(__apple_build_version__)
+
+// pick up IsDebuggerPresent()-similes for other platforms from Catch2, et al...
+
+#  include <cassert>
+#  include <sys/types.h>
+#  include <unistd.h>
+#  include <cstddef>
+#  include <ostream>
+
+// These headers will only compile with AppleClang (XCode)
+// For other compilers (Clang, GCC, ... ) we need to exclude them
+#  include <sys/sysctl.h>
+
+// The following function is taken directly from the following technical note:
+// https://developer.apple.com/library/archive/qa/qa1361/_index.html
+
+// Returns true if the current process is being debugged (either
+// running under the debugger or has a debugger attached post facto).
+DEBUGBREAK_EXTERN_C
+static inline int debugbreak_is_debugger_present(void)
+{
+	int                 mib[4];
+	struct kinfo_proc   info;
+	std::size_t         size;
+
+	// Initialize the flags so that, if sysctl fails for some bizarre
+	// reason, we get a predictable result.
+
+	info.kp_proc.p_flag = 0;
+
+	// Initialize mib, which tells sysctl the info we want, in this case
+	// we're looking for information about a specific process ID.
+
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_PROC;
+	mib[2] = KERN_PROC_PID;
+	mib[3] = getpid();
+
+	// Call sysctl.
+
+	size = sizeof(info);
+	if (sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, nullptr, 0) != 0) {
+		//Catch::cerr() << "\n** Call to sysctl failed - unable to determine if debugger is active **\n\n" << std::flush;
+		return false;
+	}
+
+	// We're being debugged if the P_TRACED flag is set.
+
+	return ((info.kp_proc.p_flag & P_TRACED) != 0);
+}
+
+#elif defined(linux) || defined(__linux) || defined(__linux__)
+
+// pick up IsDebuggerPresent()-similes for other platforms from Catch2, et al...
+
+// The standard POSIX way of detecting a debugger is to attempt to
+// ptrace() the process, but this needs to be done from a child and not
+// this process itself to still allow attaching to this process later
+// if wanted, so is rather heavy. Under Linux we have the PID of the
+// "debugger" (which doesn't need to be gdb, of course, it could also
+// be strace, for example) in /proc/$PID/status, so just get it from
+// there instead.
+//
+// https://stackoverflow.com/questions/3596781/how-to-detect-if-the-current-process-is-being-run-by-gdb
+// and the Catch2 codebase: we're going for the C implementation as that works better for our header-only approach.
+
+#if 0   // Catch2 code
+#include <fstream>
+#include <string>
+
+DEBUGBREAK_EXTERN_C
+static inline int debugbreak_is_debugger_present(void)
+{
+	// Libstdc++ has a bug, where std::ifstream sets errno to 0
+	// This way our users can properly assert over errno values
+	ErrnoGuard guard;
+	std::ifstream in("/proc/self/status");
+	for (std::string line; std::getline(in, line); ) {
+		static const int PREFIX_LEN = 11;
+		if (line.compare(0, PREFIX_LEN, "TracerPid:\t") == 0) {
+			// We're traced if the PID is not 0 and no other PID starts
+			// with 0 digit, so it's enough to check for just a single
+			// character.
+			return line.length() > PREFIX_LEN && line[PREFIX_LEN] != '0';
+		}
+	}
+
+	return false;
+}
+
+#else // S.O. + fixes from SO comments
+
+#include <sys/stat.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
+
+DEBUGBREAK_EXTERN_C
+static inline int debugbreak_is_debugger_present(void)
+{
+	char buf[4096];
+
+	const int status_fd = open("/proc/self/status", O_RDONLY);
+	if (status_fd == -1)
+		return false;
+
+	const ssize_t num_read = read(status_fd, buf, sizeof(buf) - 1);
+	close(status_fd);
+
+	if (num_read <= 0)
+		return false;
+
+	buf[num_read] = '\0';
+	constexpr char tracerPidString[] = "TracerPid:";
+	const auto tracer_pid_ptr = strstr(buf, tracerPidString);
+	if (!tracer_pid_ptr)
+		return false;
+
+	for (const char* characterPtr = tracer_pid_ptr + sizeof(tracerPidString) - 1; characterPtr <= buf + num_read; ++characterPtr)
+	{
+		if (isspace(*characterPtr))
+			continue;
+		else
+			return isdigit(*characterPtr) != 0 && *characterPtr != '0';
+	}
+
+	return false;
+}
+
+#endif
+
+#else
+
+// we don't know how to check if debugger is attached for this platform:
+// --> be conservative:
+// apply a heuristic: debug builds expect debugging, release builds DO NOT.
+DEBUGBREAK_EXTERN_C
+static inline int debugbreak_is_debugger_present(void)
+{
+#if defined(_DEBUG)
+	return true;
+#else
+	return false;
+#endif
+}
+
+#endif
+
+
+
 
 // ---------------------------------------------------------------------------------
 //
@@ -331,29 +520,41 @@ DEBUGBREAK_EXTERN_C
 __attribute__((always_inline))
 __inline__ static void debug_break(void)
 {
-	 /* SIGTRAP */
-	INLINE_SYS_RAISE(5);
+	if (debugbreak_is_debugger_present())
+	{
+		/* SIGTRAP */
+		INLINE_SYS_RAISE(5);
+	}
 }
 #elif DEBUG_BREAK_IMPL == DEBUG_BREAK_USE_TRAP_INSTRUCTION
 DEBUGBREAK_EXTERN_C
 __attribute__((always_inline))
 __inline__ static void debug_break(void)
 {
-	trap_instruction();
+	if (debugbreak_is_debugger_present())
+	{
+		trap_instruction();
+	}
 }
 #elif DEBUG_BREAK_IMPL == DEBUG_BREAK_USE_BUILTIN_DEBUGTRAP
 DEBUGBREAK_EXTERN_C
 __attribute__((always_inline))
 __inline__ static void debug_break(void)
 {
-	__builtin_debugtrap();
+	if (debugbreak_is_debugger_present())
+	{
+		__builtin_debugtrap();
+	}
 }
 #elif DEBUG_BREAK_IMPL == DEBUG_BREAK_USE_BUILTIN_TRAP
 DEBUGBREAK_EXTERN_C
 __attribute__((always_inline))
 __inline__ static void debug_break(void)
 {
-	__builtin_trap();
+	if (debugbreak_is_debugger_present())
+	{
+		__builtin_trap();
+	}
 }
 #elif DEBUG_BREAK_IMPL == DEBUG_BREAK_USE_SIGTRAP
 #include <signal.h>
@@ -362,13 +563,19 @@ DEBUGBREAK_EXTERN_C
 __attribute__((always_inline))
 __inline__ static void debug_break(void)
 {
-	raise(SIGTRAP);
+	if (debugbreak_is_debugger_present())
+	{
+		raise(SIGTRAP);
+	}
 }
 #elif DEBUG_BREAK_IMPL == DEBUG_BREAK_USE_INTRINSIC_MSVC_DEBUGBREAK
 DEBUGBREAK_EXTERN_C
 static inline void debug_break(void)
 {
-	__debugbreak();
+	if (debugbreak_is_debugger_present())
+	{
+		__debugbreak();
+	}
 }
 #elif DEBUG_BREAK_IMPL == DEBUG_BREAK_USE_STD_DEBUGBREAK
 DEBUGBREAK_EXTERN_C
